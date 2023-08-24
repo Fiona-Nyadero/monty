275bb526467f0cfd9e968073d0f87adcf7536805
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-24 14:27:38 +0000

    Task 6

--- swap.c
+++ swap.c
@@ -41,7 +41,7 @@ void sub(stack_t **stack, unsigned int line_number)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		free_stack(stack);
-		return;
+		exit(EXIT_FAILURE);
 	}
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_number);
