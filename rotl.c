c8f686127a34bd8d83ba36733d726171ef04334c
Stephanie514 <chepkiruistephanie514@gmail.com>
2023-08-27 15:26:48 +0000

    task 13

--- rotl.c
+++ rotl.c
@@ -9,23 +9,21 @@
 */
 void _rotl(stack_t **stack, unsigned int line_number)
 {
-	stack_t *first = *stack;
-	stack_t *second = NULL;
-	stack_t *last = NULL;
+	stack_t *first, *second, *last;
 	(void)line_number;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 		return;
-	second = (*stack)->next;
+	first = *stack;
+	second = first->next;
+	last = first;
 
-	while (first != NULL)
-	{
-		last = first;
-		first = first->next;
-	}
-	last->next = *stack;
-	(*stack)->prev = last;
-	second->prev = NULL;
+	while (last->next != NULL)
+		last = last->next;
+
+	last->next = first;
+	first->prev = last;
 	*stack = second;
-	last->next->next = NULL;
+	second->prev = NULL;
+	first->next = NULL;
 }
